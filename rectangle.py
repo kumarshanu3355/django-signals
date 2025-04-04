261e31bdd6023621fde09644116ad607dbd27a2f
Kumar Shanu <157588356+kumarshanu3355@users.noreply.github.com>
2025-04-04 14:33:19 +0530

    Add files via upload
     Created a Rectangle class with the following requirements:
    1. An instance of the Rectangle class requires length:int and width:int to be initialized.
    2. We can iterate over an instance of the Rectangle class 
    3. When an instance of the Rectangle class is iterated over, we first get its length in the format: {'length': <VALUE_OF_LENGTH>} followed by the width {width: <VALUE_OF_WIDTH>}

---
+++ rectangle.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, length: int, width: int):
+        self.length = length
+        self.width = width
+    
+    def __iter__(self):
+        """Allows iteration over the object, first returning length, then width."""
+        yield {"length": self.length}
+        yield {"width": self.width}
+
+# Example usage
+rect = Rectangle(10, 5)
+for item in rect:
+    print(item)
